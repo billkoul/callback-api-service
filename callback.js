f755192090e94d09054bf3a6d8bbc5e451f209f7
Vasileios Kouliaridis <billkoul8@gmail.com>
2019-02-19 17:53:05 +0200

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+var mysql = require('mysql');
+var datetime = require('node-datetime');
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var con = mysql.createPool({
+  host: "localhost",
+  user: "MY_USER",
+  password: "MY_PASSWORD",
+  database: "MY_DATABASE"
+});
+
+app.post('/', function (req, res) {
+        res.end("ACK");
+
+		check_some_value = 0;
+        if(req.body.MY_VAR == 'MY_VALUE')
+			check_some_value = 1;
+
+        con.query("MY UPDATE QUERY", function (err, result, fields) {
+			if (err) throw err;
+			console.log(result);
+		  });
+});
+
+app.listen(3000, function () {
+  console.log('DLR service listening on port 3000!')
+})
